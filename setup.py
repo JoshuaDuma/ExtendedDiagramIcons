c1d0efcd797f753cbc381825b525d294cf56f79f
joshua_duma@hotmail.com <joshua@dumahomes.com>
2024-02-06 14:54:35 -0500

    fix: resources folder

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ def get_version():
         exec(f.read(), locals)
         return locals["__version__"]
 
-def get_reources():
-    basedir = os.path.dirname(__file__)
-    return os.path.join(basedir, "/resources/**/*.png")
-
 setup(
     name="ExtendedDiagramIcons",
     version=get_version(),
@@ -23,7 +19,7 @@ setup(
     url="",
     include_package_data=True,
     package_data={
-        'ExtendedDiagramIcons': [get_reources()],  # Include only .png files in some_folder
+        'ExtendedDiagramIcons': ['../resources/**/*.png'],  # Include only .png files in some_folder
     },
     repository="https://github.com/JoshuaDuma/ExtendedDiagramIcons",
     packages=find_packages(),
